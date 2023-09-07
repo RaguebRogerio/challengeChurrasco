8b1093f642bda3f2a7d1f01c4d44050c2a3053b7
unknown <francoasfoura@alu.frt.utn.edu.ar>
2023-09-06 22:24:09 -0300

    Agrego el swagger al proyecto

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+  .setTitle('Challenge Churrasco')
+  .setDescription('Este es un challenge desarrollado por Franco Asfoura')
+  .setVersion('1.0')
+  .addTag('logging')
+  .addTag('product')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
