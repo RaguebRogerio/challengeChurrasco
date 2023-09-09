4930a805e85cd5caae4fa450deca3941b7141ea9
unknown <francoasfoura@alu.frt.utn.edu.ar>
2023-09-09 20:33:59 -0300

    Sumo las descripciones de cada endpoint al swagger

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller,  Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginAuthDto } from './dto/login-auth.dto';
-import {ApiTags} from '@nestjs/swagger'
+import {ApiTags,ApiOperation} from '@nestjs/swagger'
 
 @ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiOperation({  description: 'This service returned the token for the user. The user has the possibility of using their email or username' })
   @Post('login')
   loginUser(@Body() userObjectLogin: LoginAuthDto) {
     return this.authService.login(userObjectLogin);
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Post, Body, Param, UseGuards } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
-import {ApiBearerAuth, ApiTags} from '@nestjs/swagger'
+import {ApiBearerAuth, ApiTags, ApiOperation} from '@nestjs/swagger'
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @ApiBearerAuth()
@@ -10,36 +10,42 @@ import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
+  @ApiOperation({  description: 'This service is for create at product' })
   @UseGuards(JwtAuthGuard)
   @Post("")
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
+  @ApiOperation({  description: 'This service returns all products' })
   @UseGuards(JwtAuthGuard)
   @Get("")
   findAll() {
     return this.productsService.findAll();
   }
 
+  @ApiOperation({  description: 'This service return the product with a specific SKU' })
   @UseGuards(JwtAuthGuard)
   @Get(':sku')
   getBySKU(@Param('sku') sku: string) {
     return this.productsService.getBySKU(sku);
   }
 
+  @ApiOperation({  description: 'This service returns the product according to its name' })
   @UseGuards(JwtAuthGuard)
   @Get('/get-by-name/:name')
   getByName(@Param('name') name: string) {
     return this.productsService.getByName(name);
   }
   
+  @ApiOperation({  description: 'This service returns all products according to your currency' })
   @UseGuards(JwtAuthGuard)
   @Get('/get-by-currency/:currency')
   getAllByCurrency(@Param('currency') currency: string) {
     return this.productsService.getAllByCurrency(currency);
   }
 
+  @ApiOperation({  description: 'This service returns all products whose price is lower than the indicated one' })
   @UseGuards(JwtAuthGuard)
   @Get('/get-of-under-price/:price/:currency')
   getOfUnderPrice(
@@ -49,6 +55,7 @@ export class ProductsController {
     return this.productsService.getOfUnderPrice(price,currency);
   }
 
+  @ApiOperation({  description: 'This service returns all products whose price is higher than the indicated one' })
   @UseGuards(JwtAuthGuard)
   @Get('/get-of-over-price/:price/:currency')
   getOfOverPrice(
@@ -58,6 +65,7 @@ export class ProductsController {
     return this.productsService.getOfOverPrice(price,currency);
   }
 
+  @ApiOperation({  description: 'This service returns products within a price range' })
   @UseGuards(JwtAuthGuard)
   @Get('/get-products-in-interval-price/:minprice/:maxprice/:currency')
   getProductsInIntervalPrice(
